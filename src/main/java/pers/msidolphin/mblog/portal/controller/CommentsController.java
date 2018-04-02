3b883deb1ef3fdce4283c43c61964f1827b9d471
msidolphin <OoMass-EffectoO@hotmail.com>
2018-04-02 20:12:17 +0800

    完成评论回复接口

--- src/main/java/pers/msidolphin/mblog/portal/controller/CommentsController.java
+++ src/main/java/pers/msidolphin/mblog/portal/controller/CommentsController.java
@@ -5,10 +5,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pers.msidolphin.mblog.common.ServerResponse;
+import pers.msidolphin.mblog.object.dto.PortalUserDto;
 import pers.msidolphin.mblog.object.po.Comment;
 import pers.msidolphin.mblog.object.po.Reply;
 import pers.msidolphin.mblog.service.CommentService;
 import pers.msidolphin.mblog.service.RepliesService;
+import pers.msidolphin.mblog.service.UserService;
 
 import javax.servlet.http.HttpSession;
 
@@ -25,13 +27,20 @@ public class CommentsController {
 	@Autowired
 	private RepliesService repliesService;
 
+	@Autowired
+	private UserService userService;
+
 	@PostMapping("")
 	public ServerResponse<?> save(Comment comment) {
+		PortalUserDto user = userService.checkLogin();
+		if(user == null) return ServerResponse.unauthorized();
 		return ServerResponse.success(commentService.addComment(comment));
 	}
 
 	@PostMapping("/replies")
 	public ServerResponse<?> reply(Reply reply) {
+		PortalUserDto user = userService.checkLogin();
+		if(user == null) return ServerResponse.unauthorized();
 		return repliesService.addReplies(reply);
 	}
 
--- src/main/java/pers/msidolphin/mblog/service/RepliesService.java
+++ src/main/java/pers/msidolphin/mblog/service/RepliesService.java
@@ -3,8 +3,13 @@ package pers.msidolphin.mblog.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pers.msidolphin.mblog.common.ServerResponse;
+import pers.msidolphin.mblog.helper.AutoIdHelper;
+import pers.msidolphin.mblog.helper.RequestHolder;
 import pers.msidolphin.mblog.model.repository.RepliesRepository;
 import pers.msidolphin.mblog.object.po.Reply;
+import pers.msidolphin.mblog.object.po.User;
+
+import java.util.Date;
 
 /**
  * Created by msidolphin on 2018/3/29.
@@ -15,8 +20,23 @@ public class RepliesService {
 	@Autowired
 	private RepliesRepository repliesRepository;
 
-
 	public ServerResponse<?> addReplies(Reply reply) {
+		//评论人
+		User user = RequestHolder.getCurrentUser();
+		if (user == null || user.getId() == null) {
+			return ServerResponse.unauthorized();
+		}
+		//主键
+		reply.setId(AutoIdHelper.getId());
+		//回复人id
+		reply.setUserId(user.getId());
+		//回复时间
+		reply.setCreateTime(new Date());
+		//更新时间
+		reply.setUpdateTime(new Date());
+		//点赞数
+		reply.setVote(0);
+		repliesRepository.save(reply);
 		return ServerResponse.success(reply);
 	}
 
