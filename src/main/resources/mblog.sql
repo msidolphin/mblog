1b8acb1444d46b21c8a13363bb226069a63521b2
msidolphin <OoMass-EffectoO@hotmail.com>
2018-05-12 23:27:02 +0800

    '更新数据库表脚本'

--- src/main/resources/mblog.sql
+++ src/main/resources/mblog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 2018-03-26 14:48:40
+-- Generation Time: 2018-05-12 15:22:08
 -- 服务器版本： 5.7.14
 -- PHP Version: 5.6.25
 
@@ -30,25 +30,39 @@ USE `mblog`;
 
 DROP TABLE IF EXISTS `article`;
 CREATE TABLE IF NOT EXISTS `article` (
-  `id` int(11) NOT NULL COMMENT '主键',
+  `id` bigint(11) NOT NULL COMMENT '主键',
   `title` varchar(60) NOT NULL COMMENT '文章标题',
-  `tags` varchar(100) NOT NULL COMMENT '标签',
+  `tags` varchar(100) DEFAULT NULL COMMENT '标签',
   `type` tinyint(11) NOT NULL COMMENT '类型 0-原创 1-转载 2-翻译',
-  `thumbnail` varchar(255) NOT NULL COMMENT '缩略图',
+  `thumbnail` varchar(255) DEFAULT NULL COMMENT '缩略图',
   `content` mediumtext NOT NULL COMMENT '文章内容',
-  `is_detete` tinyint(4) DEFAULT '0' COMMENT '文章是否删除 0-否 1-是',
-  `views` int(11) NOT NULL DEFAULT '0' COMMENT '文章查阅数',
+  `is_delete` tinyint(4) DEFAULT '0' COMMENT '文章是否删除 0-否 1-是',
+  `views` int(11) DEFAULT '0' COMMENT '文章查阅数',
   `cid` int(11) DEFAULT NULL COMMENT '分类id',
   `editor` tinyint(4) DEFAULT '0' COMMENT '编辑器类型 0-富文本 1-markdown',
   `summary` varchar(255) DEFAULT NULL COMMENT '文章摘要',
-  `vote` int(11) NOT NULL DEFAULT '0' COMMENT '点赞数',
+  `vote` int(11) DEFAULT '0' COMMENT '点赞数',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
-  `creator` int(11) NOT NULL COMMENT '创建者',
-  `updator` int(11) NOT NULL COMMENT '更新者',
-  `is_delete` int(11) DEFAULT NULL,
+  `creator` int(11) DEFAULT NULL COMMENT '创建者',
+  `updator` int(11) DEFAULT NULL COMMENT '更新者',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='文章表';
+
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `article_tag`
+--
+
+DROP TABLE IF EXISTS `article_tag`;
+CREATE TABLE IF NOT EXISTS `article_tag` (
+  `aid` bigint(20) NOT NULL,
+  `tid` bigint(20) NOT NULL,
+  PRIMARY KEY (`aid`,`tid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 -- --------------------------------------------------------
 
@@ -58,11 +72,11 @@ CREATE TABLE IF NOT EXISTS `article` (
 
 DROP TABLE IF EXISTS `comment`;
 CREATE TABLE IF NOT EXISTS `comment` (
-  `id` int(11) NOT NULL COMMENT '主键',
-  `article_id` int(11) NOT NULL COMMENT '所属文章id',
-  `user_id` int(11) NOT NULL COMMENT '评论人id',
+  `id` bigint(20) NOT NULL COMMENT '主键',
+  `article_id` bigint(20) NOT NULL COMMENT '所属文章id',
+  `user_id` bigint(20) NOT NULL COMMENT '评论人id',
   `content` text NOT NULL COMMENT '评论内容',
-  `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '评论状态 0-正常 1-删除',
+  `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '评论状态 1-正常 0-删除',
   `vote` int(11) NOT NULL DEFAULT '0' COMMENT '点赞数',
   `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
@@ -70,6 +84,7 @@ CREATE TABLE IF NOT EXISTS `comment` (
   `updator` int(11) DEFAULT NULL COMMENT '更新者',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='评论表';
+
 
 -- --------------------------------------------------------
 
@@ -79,17 +94,18 @@ CREATE TABLE IF NOT EXISTS `comment` (
 
 DROP TABLE IF EXISTS `links`;
 CREATE TABLE IF NOT EXISTS `links` (
-  `id` int(11) NOT NULL COMMENT '主键',
+  `id` bigint(11) NOT NULL COMMENT '主键',
   `name` varchar(30) NOT NULL COMMENT '链接名',
   `url` varchar(255) CHARACTER SET utf32 NOT NULL COMMENT 'URL',
   `sort` int(11) NOT NULL COMMENT '排序号',
   `is_delete` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否删除 0-否 1-是',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `udpate_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
+  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
   `creator` int(11) DEFAULT NULL COMMENT '创建者',
   `updator` int(11) DEFAULT NULL COMMENT '更新者',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='友情链接';
+
 
 -- --------------------------------------------------------
 
@@ -99,7 +115,7 @@ CREATE TABLE IF NOT EXISTS `links` (
 
 DROP TABLE IF EXISTS `oauth`;
 CREATE TABLE IF NOT EXISTS `oauth` (
-  `id` int(11) NOT NULL COMMENT '主键',
+  `id` bigint(11) NOT NULL COMMENT '主键',
   `uid` int(11) NOT NULL COMMENT '关联系统用户表',
   `oauth_id` varchar(255) NOT NULL COMMENT '第三方用户id',
   `access_token` varchar(255) NOT NULL COMMENT 'access_token',
@@ -118,11 +134,12 @@ CREATE TABLE IF NOT EXISTS `oauth` (
 
 DROP TABLE IF EXISTS `replies`;
 CREATE TABLE IF NOT EXISTS `replies` (
-  `id` int(10) UNSIGNED NOT NULL COMMENT '主键',
-  `comment_id` int(10) UNSIGNED NOT NULL COMMENT '回复所属的评论id',
-  `user_id` int(10) UNSIGNED NOT NULL COMMENT '回复人id',
+  `id` bigint(20) UNSIGNED NOT NULL COMMENT '主键',
+  `comment_id` bigint(20) UNSIGNED NOT NULL COMMENT '回复所属的评论id',
+  `user_id` bigint(20) UNSIGNED NOT NULL COMMENT '回复人id',
+  `to_user_id` bigint(20) DEFAULT NULL COMMENT '回复给',
   `content` text NOT NULL COMMENT '回复内容',
-  `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '状态 0-正常 1-删除',
+  `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '状态 1-正常 0-删除',
   `vote` int(11) NOT NULL COMMENT '点赞数',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '回复时间',
   `update_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
@@ -130,6 +147,7 @@ CREATE TABLE IF NOT EXISTS `replies` (
   `updator` int(11) DEFAULT NULL COMMENT '更新者',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='回复表';
+
 
 -- --------------------------------------------------------
 
@@ -139,11 +157,12 @@ CREATE TABLE IF NOT EXISTS `replies` (
 
 DROP TABLE IF EXISTS `setting`;
 CREATE TABLE IF NOT EXISTS `setting` (
-  `id` int(11) NOT NULL COMMENT '主键',
+  `id` bigint(11) NOT NULL COMMENT '主键',
   `key_value` varchar(255) NOT NULL COMMENT '配置项key值',
   `value` text NOT NULL COMMENT '配置项值',
   `name` varchar(255) NOT NULL COMMENT '显示名称',
   `type` tinyint(4) NOT NULL COMMENT '类型 0-门户 1-后台 2-公共',
+  `is_image` tinyint(4) NOT NULL COMMENT '是否为图片 0-否 1-是',
   `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
   `creator` int(11) DEFAULT NULL COMMENT '创建者',
@@ -152,19 +171,7 @@ CREATE TABLE IF NOT EXISTS `setting` (
   UNIQUE KEY `setting_value_unqiue` (`key_value`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='站点配置表';
 
---
--- 转存表中的数据 `setting`
---
 
-INSERT INTO `setting` (`id`, `key_value`, `value`, `name`, `type`, `create_time`, `update_time`, `creator`, `updator`) VALUES
-(1, 'PORTAL_TITLE', '想要有空余时间，就不要浪费时间', '门户标题', 0, '2018-03-26 22:15:11', '2018-03-26 22:15:11', NULL, NULL),
-(2, 'PORTAL_BACKGROUND', '/static/templates/images/banner.jpg', '门户背景图', 0, '2018-03-26 22:15:11', '2018-03-26 22:15:11', NULL, NULL),
-(3, 'ICP', '粤ICP备xxxxxxxx号-x', 'ICP备案', 2, '2018-03-26 22:15:11', '2018-03-26 22:15:11', NULL, NULL),
-(4, 'SITE', 'https://github.com/msidolphin/', '站点网址', 2, '2018-03-26 22:15:11', '2018-03-26 22:15:11', NULL, NULL),
-(5, 'DESIGNED_BY', 'http://vinceok.com;vinceok', '站点模板引用', 0, '2018-03-26 22:15:11', '2018-03-26 22:15:11', NULL, NULL),
-(6, 'COPYRIGHT', '2017-2018', '版权期限', 0, '2018-03-26 22:15:11', '2018-03-26 22:15:11', NULL, NULL),
-(7, 'REPRODUCE', '除特别注明外，本站所有文章均为原创，转载请注明原文链接：', '转载说明', 0, '2018-03-26 22:15:11', '2018-03-26 22:15:11', NULL, NULL),
-(8, 'CONTRAT_EMAIL', '1226771922@qq.com', '联系邮箱', 0, '2018-03-26 22:15:11', '2018-03-26 22:15:11', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -173,18 +180,18 @@ INSERT INTO `setting` (`id`, `key_value`, `value`, `name`, `type`, `create_time`
 --
 
 DROP TABLE IF EXISTS `tags`;
-CREATE TABLE IF NOT EXISTS `tags` (
-  `id` int(11) NOT NULL COMMENT '主键',
+CREATE TABLE IF NOT EXISTS `tags` (r
+  `id` bigint(11) NOT NULL COMMENT '主键',
   `name` varchar(20) NOT NULL COMMENT '标签名称',
   `frequency` int(11) NOT NULL COMMENT '引用频率',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
   `creator` int(11) DEFAULT NULL COMMENT '创建者',
   `updator` int(11) DEFAULT NULL COMMENT '更新者',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='标签表';
 
--- --------------------------------------------------------
 
 --
 -- 表的结构 `user`
@@ -192,7 +199,7 @@ CREATE TABLE IF NOT EXISTS `tags` (
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) NOT NULL COMMENT '主键',
+  `id` bigint(20) NOT NULL COMMENT '主键',
   `username` varchar(30) NOT NULL COMMENT '用户名',
   `nickname` varchar(30) DEFAULT NULL COMMENT '昵称',
   `password` varchar(50) DEFAULT NULL COMMENT '密码',
@@ -204,15 +211,22 @@ CREATE TABLE IF NOT EXISTS `user` (
   `login_time` datetime DEFAULT NULL COMMENT '最后登录时间',
   `login_ip` varchar(40) DEFAULT NULL COMMENT '最后登录ip',
   `status` tinyint(4) DEFAULT '0' COMMENT '状态 0-启用 1-禁用',
-  `is_admin` tinyint(4) DEFAULT '1' COMMENT '是否是博主 0-是 1-否',
+  `is_admin` tinyint(4) DEFAULT '0' COMMENT '是否是博主 1-是 0-否',
   `update_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
   `summary` varchar(400) CHARACTER SET utf32 DEFAULT NULL COMMENT '介绍',
   `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `creator` int(11) DEFAULT NULL COMMENT '创建者',
   `updator` int(11) DEFAULT NULL COMMENT '更新者',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `user_username_unique` (`username`)
+  UNIQUE KEY `user_username_email_u` (`username`,`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表';
+
+--
+-- 转存表中的数据 `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `nickname`, `password`, `salt`, `avatar`, `email`, `phone`, `website`, `login_time`, `login_ip`, `status`, `is_admin`, `update_time`, `summary`, `create_time`, `creator`, `updator`) VALUES(1, 'msidolphin', 'msidolphin', '0AD7EB6ECA6DF5AC7B9D5707CECF84AE', 'MIIEowIBAAKCAQEAsaDDhOsMR4vgwUFhHjR9lvhTU7eRjLruMCtWQ3l+ACIB7xbF', '2018/04/T/jpg/2AA42FC337DC353F6F8B3AF0346D3682.jpg', '1226771922@qq.com', '15363393934', NULL, NULL, NULL, 0, 1, '2018-04-06 17:37:36', NULL, '2018-03-29 22:00:45', NULL, NULL);
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
